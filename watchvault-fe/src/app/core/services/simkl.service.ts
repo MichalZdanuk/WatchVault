df3109f28d3c1f82e7907dd19480be68fa7f60da
MichalZdanuk <zdanuk_m@wp.pl>
2025-09-23 20:37:00 +0200

    Implement basics of simkl.service p2

---
+++ watchvault-fe/src/app/core/services/simkl.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Movie } from '../../shared/models/movie';
+import { TrendingInterval } from '../../shared/models/trending-interval';
+import { TrendingMovie } from '../../shared/models/trending-movie';
+import { MovieDetails } from '../../shared/models/movie-details';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SimklService {
+  private apiUrl = environment.apiUrl + '/simkl/movies';
+
+  constructor(private http: HttpClient) {}
+
+  getMovies(searchTerm: string, page: number = 1, pageSize: number = 10): Observable<Movie[]> {
+    const params = new HttpParams()
+      .set('searchTerm', searchTerm)
+      .set('page', page)
+      .set('pageSize', pageSize);
+
+    return this.http.get<Movie[]>(`${this.apiUrl}/search`, { params });
+  }
+
+  getMovieDetails(simklId: number): Observable<MovieDetails> {
+    return this.http.get<MovieDetails>(`${this.apiUrl}/${simklId}`);
+  }
+
+  getTrendingMovies(trendingInterval: TrendingInterval): Observable<TrendingMovie[]> {
+    const params = new HttpParams().set('trendingInterval', trendingInterval);
+
+    return this.http.get<TrendingMovie[]>(`${this.apiUrl}/trending`, { params });
+  }
+}
---
+++ watchvault-fe/src/app/shared/models/trending-interval.ts
@@ -0,0 +1,5 @@
+export enum TrendingInterval {
+  Today,
+  Week,
+  Month,
+}
