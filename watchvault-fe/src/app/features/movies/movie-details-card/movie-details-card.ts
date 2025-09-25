7872ef39c165b3653e3b05c66fa123ca87f5353f
MichalZdanuk <zdanuk_m@wp.pl>
2025-09-25 23:03:38 +0200

    Add binding for actions and routing on movie details card

--- watchvault-fe/src/app/features/movies/movie-details-card/movie-details-card.ts
+++ watchvault-fe/src/app/features/movies/movie-details-card/movie-details-card.ts
@@ -3,6 +3,7 @@ import { MovieDetails } from '../../../shared/models/movie-details';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details-card',
@@ -12,4 +13,18 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class MovieDetailsCard {
   @Input() movieDetails!: MovieDetails;
+
+  constructor(private router: Router) {}
+
+  addToWatchlist(simklId: number): void {
+    console.log(`Adding movie: ${simklId}`);
+
+    this.router.navigate(['/profile']);
+  }
+
+  markWatched(simklId: number): void {
+    console.log(`Marking as watched movie: ${simklId}`);
+
+    this.router.navigate(['/profile']);
+  }
 }
