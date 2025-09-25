68d0db206802591a65ccc133200e980696d573e9
MichalZdanuk <zdanuk_m@wp.pl>
2025-09-25 22:56:31 +0200

    Refactor movie not found compononent fragment

--- watchvault-fe/src/app/features/movies/pages/movie-details.component/movie-details.component.ts
+++ watchvault-fe/src/app/features/movies/pages/movie-details.component/movie-details.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { MovieDetails } from '../../../../shared/models/movie-details';
 import { SimklService } from '../../../../core/services/simkl.service';
 import { CommonModule } from '@angular/common';
 import { MovieDetailsCard } from '../../movie-details-card/movie-details-card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-movie-details.component',
-  imports: [CommonModule, MovieDetailsCard],
+  imports: [CommonModule, MovieDetailsCard, MatIconModule, RouterLink],
   templateUrl: './movie-details.component.html',
   styleUrl: './movie-details.component.css',
 })
