131d214392d50a2b3c5dd7516a0dff19ac178267
MichalZdanuk <zdanuk_m@wp.pl>
2025-09-26 11:07:21 +0200

    Add snackbar in MovieDetailsCard

--- watchvault-fe/src/app/features/movies/movie-details-card/movie-details-card.ts
+++ watchvault-fe/src/app/features/movies/movie-details-card/movie-details-card.ts
@@ -3,29 +3,40 @@ import { MovieDetails } from '../../../shared/models/movie-details';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { MovieRecommendations } from '../movie-recommendations/movie-recommendations';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-details-card',
-  imports: [MatCardModule, MatIconModule, CommonModule, RouterLink, MovieRecommendations],
+  imports: [MatCardModule, MatIconModule, MatSnackBarModule, CommonModule, MovieRecommendations],
   templateUrl: './movie-details-card.html',
   styleUrl: './movie-details-card.css',
 })
 export class MovieDetailsCard {
   @Input() movieDetails!: MovieDetails;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private snackBar: MatSnackBar) {}
 
   addToWatchlist(simklId: number): void {
     console.log(`Adding movie: ${simklId}`);
 
-    this.router.navigate(['/profile']);
+    this.snackBar.open('Added to watchlist', '', {
+      panelClass: ['custom-snackbar'],
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
   }
 
   markWatched(simklId: number): void {
     console.log(`Marking as watched movie: ${simklId}`);
 
-    this.router.navigate(['/profile']);
+    this.snackBar.open('Marked as watched', '', {
+      panelClass: ['custom-snackbar'],
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
   }
 }
