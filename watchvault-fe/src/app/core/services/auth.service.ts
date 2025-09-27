26ebb7d6b5f5af61d1ca1d9c902f936d4a9f12c3
MichalZdanuk <zdanuk_m@wp.pl>
2025-09-27 17:42:05 +0200

    Implement LoginPage

--- watchvault-fe/src/app/core/services/auth.service.ts
+++ watchvault-fe/src/app/core/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Register } from '../../shared/models/register';
 import { Observable } from 'rxjs';
+import { LoginResponse } from '../../shared/models/loginResponse';
+import { Login } from '../../shared/models/login';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +16,11 @@ export class AuthService {
 
   register(register: Register): Observable<void> {
     return this.http.post<void>(this.apiUrl + '/register', register);
+  }
+
+  login(login: Login): Observable<LoginResponse> {
+    const params = new HttpParams().set('username', login.username).set('password', login.password);
+
+    return this.http.get<LoginResponse>(this.apiUrl + '/retrieve-token', { params });
   }
 }
--- watchvault-fe/src/app/features/auth/pages/login.component/login.component.ts
+++ watchvault-fe/src/app/features/auth/pages/login.component/login.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../../core/services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { Login } from '../../../../shared/models/login';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login.component',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({});
+  errorMessage: string = '';
 
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    let login: Login = this.loginForm.value;
+
+    this.authService.login(login).subscribe({
+      next: () => {
+        this.router.navigate(['/movies/trending']);
+      },
+      error: () => {
+        this.errorMessage = 'Invalid login credentials.';
+      },
+    });
+  }
 }
