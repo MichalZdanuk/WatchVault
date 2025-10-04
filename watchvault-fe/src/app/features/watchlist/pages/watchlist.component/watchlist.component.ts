560a2a23e79e55876c8d41962e1bfb5629cc9ea0
MichalZdanuk <zdanuk_m@wp.pl>
2025-10-04 18:58:01 +0200

    Reorder watchlist page

--- watchvault-fe/src/app/features/watchlist/pages/watchlist.component/watchlist.component.ts
+++ watchvault-fe/src/app/features/watchlist/pages/watchlist.component/watchlist.component.ts
@@ -4,10 +4,11 @@ import { WatchlistSummary } from '../../watchlist-summary/watchlist-summary';
 import { WatchlistItems } from '../../watchlist-items/watchlist-items';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
+import { InfoIcon } from '../../../../shared/components/info-icon/info-icon';
 
 @Component({
   selector: 'app-watchlist.component',
-  imports: [WatchlistSummary, WatchlistItems, MatTabsModule, CommonModule],
+  imports: [InfoIcon, WatchlistSummary, WatchlistItems, MatTabsModule, CommonModule],
   templateUrl: './watchlist.component.html',
   styleUrl: './watchlist.component.css',
 })
