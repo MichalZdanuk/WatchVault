71b5c627591b82e286a2d88a02745b08d15bfb6c
MichalZdanuk <zdanuk_m@wp.pl>
2025-10-21 11:53:07 +0200

    Implement simple caching of user history.

--- WatchVault/src/WatchVault.Application/Queries/BrowseWatchHistory/BrowseWatchHistoryQueryHandler.cs
+++ WatchVault/src/WatchVault.Application/Queries/BrowseWatchHistory/BrowseWatchHistoryQueryHandler.cs
@@ -1,38 +1,43 @@
-﻿using WatchVault.Application.Common;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using WatchVault.Application.Common;
 using WatchVault.Application.Enums;
 using WatchVault.Application.Repositories;
 
 namespace WatchVault.Application.Queries.BrowseWatchHistory;
 public sealed class BrowseWatchHistoryQueryHandler(IUserContext userContext,
+    IDistributedCache cache,
     IUnitOfWork unitOfWork) : IQueryHandler<BrowseWatchHistoryQuery, WatchListHistoryDto>
 {
     public async Task<WatchListHistoryDto> Handle(BrowseWatchHistoryQuery query, CancellationToken cancellationToken)
     {
         var userId = userContext.UserId;
+        var cacheKey = $"history-{userId}-{query.PageNumber}-{query.PageSize}";
 
-        var items = await unitOfWork.WatchListRepository.GetWatchlistHistoryAsync(userId, query.PageNumber, query.PageSize);
+        var result = await cache.GetOrSetAsync(cacheKey, async () =>
+        {
+            var items = await unitOfWork.WatchListRepository
+                .GetWatchlistHistoryAsync(userId, query.PageNumber, query.PageSize);
 
-        IReadOnlyList<WatchHistoryItemDto> dtos = items.Select(x =>
-            new WatchHistoryItemDto(
-                x.Id,
-                x.AddedToWatchAt,
-                x.WatchedAt,
-                x.IsFavourite,
-                x.Movie.SimklId,
-                x.Movie.Title,
-                x.Movie.PosterUrl,
-                x.Movie.Genres,
-                x.WatchStatus.ConvertToApplicationWatchStatus()
-            )
-        ).ToList();
+            var dtos = items.Select(x =>
+                new WatchHistoryItemDto(
+                    x.Id,
+                    x.AddedToWatchAt,
+                    x.WatchedAt,
+                    x.IsFavourite,
+                    x.Movie.SimklId,
+                    x.Movie.Title,
+                    x.Movie.PosterUrl,
+                    x.Movie.Genres,
+                    x.WatchStatus.ConvertToApplicationWatchStatus()
+                )
+            ).ToList();
 
-        var totalCount = await unitOfWork.WatchListRepository.GetWatchlistHistoryCountAsync(userId);
+            var totalCount = await unitOfWork.WatchListRepository
+                .GetWatchlistHistoryCountAsync(userId);
 
-        return new WatchListHistoryDto(
-            query.PageNumber,
-            query.PageSize,
-            totalCount,
-            dtos
-        );
+            return new WatchListHistoryDto(query.PageNumber, query.PageSize, totalCount, dtos);
+        });
+
+        return result ?? new WatchListHistoryDto(query.PageNumber, query.PageSize, 0, []);
     }
 }
